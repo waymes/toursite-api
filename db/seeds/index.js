a5af3f3ecc82b6e49955dbc56bc59508a2590180
waymes <vladimirbar7@gmail.com>
2019-07-20 14:12:27 +0300

    turkey trip seeds + trip schema changes

--- db/seeds/index.js
+++ db/seeds/index.js
@@ -1,120 +1,4 @@
-const firstBlockItems = [
-  {
-    title: '12 дней',
-    details: 'экшна, приключений и культурных сюрпризов. Каждая ночёвка - в новом и необычном месте',
-  },
-  {
-    title: '2300 километров',
-    details: 'проедем, пролетим и проплывём по Ирану с Севера на Персидский залив и обратно',
-  },
-  {
-    title: '45 объектов',
-    details: 'мирового культурно-исторического наследия мы посетим, включая объекты ЮНЕСКО',
-  },
-];
-const secondBlockItems = [
-  {
-    image: '/static/tour/tour_picture_1.jpg',
-    text: 'Заберёмся на верхушку одной из самых высоких телебашен в мире!',
-  },
-  {
-    image: '/static/tour/tour_picture_2.jpg',
-    text: 'Ощутим себя на Луне посреди белоснежной соляной пустыни',
-  },
-  {
-    image: '/static/tour/tour_picture_3.jpg',
-    text: 'Будем кувыркаться с высоких дюн в пустыне и гонять по ним на джипе',
-  },
-  {
-    image: '/static/tour/tour_picture_4.jpg',
-    text: 'Купаться в Персидском заливе на вулканическом острове!',
-  },
-  {
-    image: '/static/tour/tour_picture_5.jpg',
-    text: 'Побываем в огромном настоящем сейфе - сокровищнице Персии!',
-  },
-  {
-    image: '/static/tour/tour_picture_5.jpg',
-    text: 'Жить в средневековом каравансарае для путников Шелкового Пути',
-  },
-  {
-    image: '/static/tour/tour_picture_5.jpg',
-    text: 'Гулять по крышам древних городов!',
-  },
-  {
-    image: '/static/tour/tour_picture_5.jpg',
-    text: 'Познакомимся с исламской культурой и невероятно открытыми людьми',
-  },
-];
-const thirdBlockItems = [
-  {
-    title: 'Идеальная страна для насыщенного путешествия, которая пока еще не стала трендом среди туристов',
-    details: 'Иран активно развивает туризм и обладает очень развитой транспортной инфраструктурой, большим количеством отелей и высокой плотностью самых разнообразных достопримечательностей.',
-  },
-  {
-    title: 'Великое историческое наследие, необычная архитектура, космические пейзажи и самые гостеприимные люди!',
-    details: 'Это одна из четырех сохранившихся до наших дней древнейших мировых цивилизаций.',
-  },
-  {
-    title: 'Иран - одна из самых безопасных стран в мире!',
-    details: 'Да-да, именно так и есть! Не путайте Иран с Ираком) В Иране нет никаких войн и террористов. Согласно международному рейтингу стран по уровню преступности - в Иране безопаснее чем в Украине, России, США, Турции и целого ряда европейских стран. Как минимум, там нет такого явления, как "гопники" :)',
-  },
-  {
-    title: 'Это единственная страна в мире, где ещё сохранился древний колорит и романтика сказочного Востока',
-    details: 'Именно в Иране происходили действия из "Сказки тысячи и одной ночи", Алладин летал на ковре-самолёте к прекрасной Жасмин, а обычные иранцы по сей день цитируют великого поэта - Омара Хайяма. В старинных городах до сих пор ощущается магия Востока.',
-  },
-  {
-    title: 'Здесь до сих пор кипят жизнью настоящие средневековые восточные базары',
-    details: 'Можно купить самые необычные вещи, разнообразные специи, сладости и пряности, знаменитые персидские ковры-самолёты, лампы Алладина и аутентичную утварь - мы советуем брать обратный билет домой с багажом:)',
-  },
-  {
-    title: 'Современный и древний, уникальный и аутентичный, микс природы и культуры',
-    details: 'Этот тур очень сложно назвать "туром", это сделано для поисковой системы Гугла:) На самом деле это - настоящее путешествие, приключение. Полное погружение в культуру.',
-  },
-];
-const fourthBlockItems = [
-  '/static/tour/tour_picture_1.jpg',
-  '/static/tour/tour_picture_2.jpg',
-  '/static/tour/tour_picture_3.jpg',
-  '/static/tour/tour_picture_4.jpg',
-  '/static/tour/tour_picture_5.jpg',
-  '/static/tour/tour_picture_6.jpg',
-];
-const fifthBlockItems = [
-  {
-    title: 'Проживание',
-    details: 'В традиционных персидских отелях, гестхаусах, настоящем средневековом каравансарае и в частном доме на острове. С завтраками.',
-    icon: 'fas fa-home',
-  },
-  {
-    title: 'Все переезды в Иране',
-    details: 'Аренда микроавтобуса/автомобиля, проезд в комфортабельном поезде "Персидский Экспресс", паромы на остров и внутренний перелёт на самолёте.',
-    icon: 'fas fa-bus',
-  },
-  {
-    title: 'Достопримечательности',
-    details: 'Входные билеты во все объекты посещения в поездке - дворцы, музеи, башни, сады, замки, мечети и мероприятия.',
-    icon: 'fas fa-place-of-worship',
-  },
-];
-const additionalExpenses = [
-  {
-    title: 'Авиаперелёт',
-    details: 'Из Вашего города в Тегеран. Поможем купить билеты.',
-  },
-  {
-    title: 'Виза',
-    details: 'Оформляется по прилёту в аэропорт Тегерана. Стоимость - 95$',
-  },
-  {
-    title: 'Страховка',
-    details: 'Иранская медицинская страховка оформляется лично по прилёту в аэропорт, 16$',
-  },
-  {
-    title: 'Питание',
-    details: 'Обеды и ужины в кафешках, примерно 8-15$/день',
-  },
-];
+import * as constants from './temp-constants';
 
 const bulkData = [
   {
@@ -122,87 +6,24 @@ const bulkData = [
     records: [
       {
         createdAt: new Date(),
-        dateFrom: new Date('2019-09-12'),
-        dateTo: new Date('2019-09-22'),
-        image: '/static/tour/tour_picture_1.jpg',
-        title: 'Открой для себя магию Востока!',
-        destination: 'Иран',
-        name: 'Тур в Иран',
-        firstBlockTitle: 'Готовьтесь к самому потрясающему приключению:',
-        firstBlockItems: JSON.stringify(firstBlockItems),
-        secondBlockTitle: 'Что нас ждёт в поездке?',
-        secondBlockItems: JSON.stringify(secondBlockItems),
-        thirdBlockTitle: 'Почему Иран?',
-        thirdBlockItems: JSON.stringify(thirdBlockItems),
-        fourthBlockTitle: 'Посмотрите, как это было в прошлый раз:',
-        fourthBlockItems: JSON.stringify(fourthBlockItems),
-        fifthBlockTitle: 'Что включено?',
-        fifthBlockItems: JSON.stringify(fifthBlockItems),
-        price: 1099,
-        additionalExpenses: JSON.stringify(additionalExpenses),
-      },
-      {
-        createdAt: new Date(),
-        dateFrom: new Date('2019-10-05'),
-        dateTo: new Date('2019-10-14'),
-        image: '/static/tour/tour_picture_2.jpg',
+        dateFrom: new Date('2019-09-20'),
+        dateTo: new Date('2019-09-29'),
+        backgroundImage: '/static/trip/turkey/background-image.jpg',
         title: 'Открой для себя магию Востока!',
         destination: 'Турция',
-        name: 'Путешествие в Турцию',
-        firstBlockTitle: 'Готовьтесь к самому потрясающему приключению:',
-        firstBlockItems: JSON.stringify(firstBlockItems),
-        secondBlockTitle: 'Что нас ждёт в поездке?',
-        secondBlockItems: JSON.stringify(secondBlockItems),
-        thirdBlockTitle: 'Почему Турция?',
-        thirdBlockItems: JSON.stringify(thirdBlockItems),
-        fourthBlockTitle: 'Посмотрите, как это было в прошлый раз:',
-        fourthBlockItems: JSON.stringify(fourthBlockItems),
-        fifthBlockTitle: 'Что включено?',
-        fifthBlockItems: JSON.stringify(fifthBlockItems),
-        price: 1199,
-        additionalExpenses: JSON.stringify(additionalExpenses),
-      },
-      {
-        createdAt: new Date(),
-        dateFrom: new Date('2019-12-15'),
-        dateTo: new Date('2019-12-27'),
-        image: '/static/tour/tour_picture_3.jpg',
-        title: 'Открой для себя магию Европы!',
-        destination: 'Болгария',
-        name: 'Тур в Болгарию',
-        firstBlockTitle: 'Готовьтесь к самому потрясающему приключению:',
-        firstBlockItems: JSON.stringify(firstBlockItems),
-        secondBlockTitle: 'Что нас ждёт в поездке?',
-        secondBlockItems: JSON.stringify(secondBlockItems),
-        thirdBlockTitle: 'Почему Болгария?',
-        thirdBlockItems: JSON.stringify(thirdBlockItems),
-        fourthBlockTitle: 'Посмотрите, как это было в прошлый раз:',
-        fourthBlockItems: JSON.stringify(fourthBlockItems),
-        fifthBlockTitle: 'Что включено?',
-        fifthBlockItems: JSON.stringify(fifthBlockItems),
-        price: 900,
-        additionalExpenses: JSON.stringify(additionalExpenses),
-      },
-      {
-        createdAt: new Date(),
-        dateFrom: new Date('2020-02-10'),
-        dateTo: new Date('2020-02-17'),
-        image: '/static/tour/tour_picture_4.jpg',
-        title: 'Открой для себя магию Карпат!',
-        destination: 'Карпаты',
-        name: 'Поход в Карпаты',
-        firstBlockTitle: 'Готовьтесь к самому потрясающему приключению:',
-        firstBlockItems: JSON.stringify(firstBlockItems),
-        secondBlockTitle: 'Что нас ждёт в поездке?',
-        secondBlockItems: JSON.stringify(secondBlockItems),
-        thirdBlockTitle: 'Почему Карпаты?',
-        thirdBlockItems: JSON.stringify(thirdBlockItems),
-        fourthBlockTitle: 'Посмотрите, как это было в прошлый раз:',
-        fourthBlockItems: JSON.stringify(fourthBlockItems),
-        fifthBlockTitle: 'Что включено?',
-        fifthBlockItems: JSON.stringify(fifthBlockItems),
-        price: 510,
-        additionalExpenses: JSON.stringify(additionalExpenses),
+        name: 'Неизведанная Турция',
+        blockTitle1: 'Готовьтесь к самому потрясающему приключению:',
+        blockItems1: JSON.stringify(constants.firstBlockItems),
+        blockTitle2: 'Что нас ждёт в поездке?',
+        blockItems2: JSON.stringify(constants.turkeyBlockItems2),
+        blockTitle3: 'Почему Турция?',
+        blockItems3: JSON.stringify(constants.turkeyBlockItems3),
+        blockTitle4: 'Посмотрите, как это было в прошлый раз:',
+        blockItems4: JSON.stringify(constants.turkeyBlockItems4),
+        price: 420,
+        includedList: JSON.stringify(constants.turkeyIncludedList),
+        additionalList: JSON.stringify(constants.turkeyAdditionalList),
+        description: constants.turkeyDescription,
       },
     ],
   },
---
+++ db/seeds/temp-constants.js
@@ -0,0 +1,129 @@
+export const firstBlockItems = [
+  {
+    title: '12 дней',
+    details: 'экшна, приключений и культурных сюрпризов. Каждая ночёвка - в новом и необычном месте',
+  },
+  {
+    title: '2300 километров',
+    details: 'проедем, пролетим и проплывём по Ирану с Севера на Персидский залив и обратно',
+  },
+  {
+    title: '45 объектов',
+    details: 'мирового культурно-исторического наследия мы посетим, включая объекты ЮНЕСКО',
+  },
+];
+
+// Turkey ==============================================
+export const turkeyBlockItems2 = [
+  {
+    image: '/static/trip/turkey/block-picture-2-1.jpg',
+    text: 'Увидим один из самых больших каньонов Европы и Малой Азии.',
+  },
+  {
+    image: '/static/trip/turkey/block-picture-2-2.jpg',
+    text: 'Рафтинг по горной реке!',
+  },
+  {
+    image: '/static/trip/turkey/block-picture-2-3.jpg',
+    text: 'Полёт на параплане с высоты 2000м',
+  },
+  {
+    image: '/static/trip/turkey/block-picture-2-4.jpg',
+    text: 'Будем жить в турецкой высокогорной деревне',
+  },
+  {
+    image: '/static/trip/turkey/block-picture-2-5.jpg',
+    text: 'Взберемся на огненную гору - место захоронения мифической Химеры.',
+  },
+  {
+    image: '/static/trip/turkey/block-picture-2-6.jpg',
+    text: 'Рай для искателей приключений - исследуем заброшенные древние города!',
+  },
+  {
+    image: '/static/trip/turkey/block-picture-2-7.jpg',
+    text: 'Живём в палатках и готовим еду на костре.',
+  },
+  {
+    image: '/static/trip/turkey/block-picture-2-8.jpg',
+    text: 'Прогулка на яхте по пиратским бухтам',
+  },
+  {
+    image: '/static/trip/turkey/block-picture-2-9.jpg',
+    text: 'Заберемся на самый высокогорный амфитеатр в мире!',
+  },
+  {
+    image: '/static/trip/turkey/block-picture-2-10.jpg',
+    text: 'Увидим турецкие Мальдивы - необычное горное озеро.',
+  },
+  {
+    image: '/static/trip/turkey/block-picture-2-11.jpg',
+    text: 'Побываем в средневековых монументальных каравансараях.',
+  },
+  {
+    image: '/static/trip/turkey/block-picture-2-12.jpg',
+    text: 'Насладимся чистым и теплым Средиземным морем',
+  },
+];
+export const turkeyBlockItems3 = [
+  `<b>Турция - одна из самых красивейших стран на нашей планете,</b> но этого не знают и не видят 
+  туристы, которые едут отдыхать в отели all-inclusive. Природа Турции - это рай на Земле.`,
+  `<b>Колоссальное историческое наследие</b> - ведь Древняя Греция была на территории современной Турции! 
+  Высокая концентрация сверхинтересных исторических локаций.`,
+  `<b>Почувствуйте себя Индианой Джонсом или Ларой Крофт</b> - мы будем исследовать захваченные природой 
+  древние города и настоящие гробницы!`,
+  `Одна из важнейших "фишек" Турции - сохранившаяся <b>аутентичность</b>, 
+  <b>колорит</b> и <b>душевность</b> нетуристических мест.`,
+  `<b>Приветливость и доброжелательность турков</b> поражает, вдохновляет и учит нас быть открытее 
+  и добрее :)`,
+  `<b>Идеальная страна для насыщенного и активного путешествия</b>, которое удивит 
+  путешественника любого уровня!`,
+];
+export const turkeyIncludedList = [
+  'Аренда автомобилей',
+  'Топливо на всю поездку',
+  'Газобалонное оборудование',
+  'Завтраки и ужины (кроме Антальи)',
+  '2 ночи в отеле Чиралы',
+  '1 ночь в деревенском доме у местных',
+  'Услуги гида',
+];
+export const turkeyAdditionalList = [
+  'Перелёт в Анталью и обратно домой (поможем с билетами)',
+  'Обеды в местных столовых/кафешках',
+  'Проживание в отеле в Анталье, 2 ночи (по приезду и перед вылетом)',
+  'Рафтинг на горной реке (20$)',
+  'Полёт на параплане над бухтами Олюдениза (90$)',
+  'Прогулка на яхте (15$)',
+  'Вход в музеи и природные достопримечательности',
+];
+export const turkeyBlockItems4 = [
+  '/static/trip/turkey/block-picture-4-1.jpg',
+  '/static/trip/turkey/block-picture-4-2.jpg',
+  '/static/trip/turkey/block-picture-4-3.jpg',
+  '/static/trip/turkey/block-picture-4-4.jpg',
+  '/static/trip/turkey/block-picture-4-5.jpg',
+  '/static/trip/turkey/block-picture-4-6.jpg',
+  '/static/trip/turkey/block-picture-4-7.jpg',
+  '/static/trip/turkey/block-picture-4-8.jpg',
+  '/static/trip/turkey/block-picture-4-9.jpg',
+  '/static/trip/turkey/block-picture-4-10.jpg',
+  '/static/trip/turkey/block-picture-4-11.jpg',
+  '/static/trip/turkey/block-picture-4-12.jpg',
+  '/static/trip/turkey/block-picture-4-13.jpg',
+  '/static/trip/turkey/block-picture-4-14.jpg',
+  '/static/trip/turkey/block-picture-4-15.jpg',
+  '/static/trip/turkey/block-picture-4-16.jpg',
+  '/static/trip/turkey/block-picture-4-17.jpg',
+  '/static/trip/turkey/block-picture-4-18.jpg',
+  '/static/trip/turkey/block-picture-4-19.jpg',
+  '/static/trip/turkey/block-picture-4-20.jpg',
+  '/static/trip/turkey/block-picture-4-21.jpg',
+  '/static/trip/turkey/block-picture-4-22.jpg',
+  '/static/trip/turkey/block-picture-4-23.jpg',
+  '/static/trip/turkey/block-picture-4-24.jpg',
+];
+export const turkeyDescription = `Сбор участников путешествия - в Анталье, организатор забирает 
+  всех без исключения из аэропорта. Мы едем за природой и аутентикой - это самое главное. 
+  Поэтому большую часть путешествия мы ночуем в палатках. Завтраки и ужины готовим на костре/горелке.
+  На нашем маршруте будет много необычных и непопсовых локаций, как природных так и рукотворных. 
+  Готовьтесь к приятным сюрпризам ;)`;
